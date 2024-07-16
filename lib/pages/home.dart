76f9a87ee5cd7c54ae9960689c655d3bbe944e05
Atharva zare <120238220+2atharva21@users.noreply.github.com>
2024-07-16 09:59:07 +0530

    #2

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,10 +7,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('flutter course')),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: const Text(
+          'flutter course ',
+          style: TextStyle(color: Colors.deepPurple), 
+          textAlign: TextAlign.center,
+        ),
+      ),
       body: Center(
         child: Container(
-          child: Text('wecome atharva zare     '),
+          child: const Text('wecome atharva zare     '),
         ),
       ),
       drawer: MyDrawer(),
